f13dcbb553ddcf5da273c3f738b51fb8831c2aa8
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-08-09 02:00:45 +0530

    fs module complete

--- index.js
+++ index.js
@@ -1,48 +1,83 @@
-const express = require("express");
-const fs = require("fs");
+import express from "express";
+import fs from "fs";
+
 const port = 8000;
-
 const app = express();
-
 app.use(express.json());
 
-const tours = JSON.parse(fs.readFileSync("./data/tours-simple.json"));
+const data = JSON.parse(fs.readFileSync("./data/tours-simple.json"));
 
-app.get("/api/tours", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      tours: tours,
-    },
-  });
+// ⛔THIS IS FOR THE POSTING⛔::
+
+app.post("/api/person", (req, res) => {
+  try {
+    const newPerson = Object.assign({ id: new Date() }, req.body);
+    data.push(newPerson);
+    fs.writeFileSync("./data/tours-simple.json", JSON.stringify(data));
+    res.send("DONE!");
+    // res.status(201).json({
+    //   data: {
+    //     data,
+    //   },
+    // });
+  } catch (err) {
+    res.status(400).json({
+      status: "Fail",
+      data: {
+        err,
+      },
+    });
+  }
 });
 
-app.post("/api/tours", (req, res) => {
-  console.log(req.body);
-  const newTours = Object.assign({ id: new Date() }, req.body);
-  tours.push(newTours);
-  fs.writeFileSync("./data/tours-simple.json", JSON.stringify(tours), (err) => {
+// ⛔THIS IS FOR GET ALL DATA::⛔
+
+app.get("/api/person", (req, res) => {
+  try {
     res.status(200).json({
       status: "success",
       data: {
-        tours: newTours,
+        data,
       },
     });
-  });
-  res.send("Done!");
+  } catch (err) {
+    res.status(400).json({
+      status: "Fail",
+      data: {
+        err,
+      },
+    });
+  }
 });
 
-app.get("/api/tours/:id", (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-  const findTours = tours.find((el) => el.id === id);
-  res.status(200).json({
-    data: {
-      tours: findTours,
-    },
+// ⛔THIS IS FOR GET DATA BY ID⛔
+
+app.get("/api/person/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    // console.log(id);
+    const findPerson = data.find((el) => el.id === id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: findPerson,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Fail",
+      data: {
+        err,
+      },
+    });
+  }
+});
+
+
+try {
+  app.listen(port, () => {
+    console.log("server running on " + port + "");
   });
-  // res.send("Done")
-});
-app.listen(port, () => {
-  console.log(`server running on ${port}...`);
-});
+} catch (err) {
+  console.log(err);
+}
