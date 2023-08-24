0ec509189027e957db5e9c4814a3be8d784130fd
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-08-24 13:43:09 +0530

    ewr

---
+++ server/controller/dataControllers.js
@@ -0,0 +1,8 @@
+import express from "express";
+import fs from "fs";
+
+const data = JSON.parse(fs.readFileSync("../data/data.json"));
+
+const post=async("/api/person",(req,res)=>{
+    
+})
--- server/index.js
+++ server/index.js
@@ -1,16 +1,69 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv"; //this line make the .env file enables
+import cors from "cors";
+import fs from "fs";
+import morgan from "morgan";
 
 dotenv.config(); // configuring the dotenv
+
 const PORT = process.env.PORT;
-const HOSTNAME=process.env.HOST_NAME;
+const HOSTNAME = process.env.HOST_NAME;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
-try {
-  app.listen(PORT, () => {
-    console.log(`server running on ${HOSTNAME}:${PORT}...`);
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "hello" });
+});
+
+const jsonData = JSON.parse(fs.readFileSync("./data/data.json"));
+app.post("/api/person", (req, res) => {
+  try {
+    const newPerson = Object.assign({ id: new Date() }, req.body);
+    jsonData.push(newPerson);
+    fs.writeFileSync("./data/data.json", JSON.stringify(jsonData));
+    res.send("DONE!");
+    // res.status(200).json({
+    //   data: {
+    //     jsonData,
+    //   },
+    // });
+  } catch (err) {
+    res.status(404).json({
+      data: err,
+    });
+  }
+});
+app.get("/api/person", (req, res) => {
+  try {
+    res.status(200).json({
+      data: {
+        jsonData,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      data: err,
+    });
+  }
+});
+app.get("/api/person/:id", (req, res) => {
+  console.log(req.params);
+  const id = req.params.id;
+  const person = jsonData.find((el) => el.id === id);
+  res.status(200).json({
+    data: {
+      person,
+    },
   });
-} catch (err) {
-  console.log(err);
-}
+});
+
+app.listen(PORT, () => {
+  try {
+    console.log(`server running on ${HOSTNAME}:${PORT}...`);
+  } catch (err) {
+    console.log(err);
+  }
+});
